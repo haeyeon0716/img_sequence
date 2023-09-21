ac5142816ccd85d373216343222473171c19c95e
yhy7805 <yhy7805@gmail.com>
2023-09-21 14:11:05 +0900

    마우스 커서 움직임에 따라 정수 출력

--- main.js
+++ main.js
@@ -12,6 +12,10 @@
 //위 작업을 100번 반복 돌리면서
 //append로 이미지 요소 반복추가
 
+//백분율 구하는 공식
+//현재 수치값 / 전체수치값 * 100 (백분율)
+//현재 수치값 / 전체수치값 * 200 (이백분율)
+
 const section = document.querySelector('section');
 
 
@@ -23,3 +27,13 @@ for(let i=0; i<=200; i++){
     img.setAttributeNode(src);
     section.append(img);
 }
+
+window.addEventListener('mousemove' , (e)=>{
+    const curPos = e.pageX;
+    const wid = window.innerWidth;
+    const percent = parseInt((curPos/wid) *200);
+    console.log(percent);
+    
+    //parseInt(숫자) : 실수에서 소수점 아랫값을 버려서 정수 반환
+    //parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
+})
