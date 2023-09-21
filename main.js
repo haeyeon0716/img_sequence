42dd27c3da34328f0deba8f5a33775e4c85a6c76
yhy7805 <yhy7805@gmail.com>
2023-09-21 14:18:15 +0900

    이미지 생성함수 개선

--- main.js
+++ main.js
@@ -17,23 +17,27 @@
 //현재 수치값 / 전체수치값 * 200 (이백분율)
 
 const section = document.querySelector('section');
-
-
-
-for(let i=0; i<=200; i++){
-    const img = document.createElement('img');
-    const src = document.createAttribute('src');
-    src.value = `img/pic${i}.jpg`;
-    img.setAttributeNode(src);
-    section.append(img);
-}
+const imgs = createImgs(section, 200);
+console.log(imgs);
 
 window.addEventListener('mousemove' , (e)=>{
     const curPos = e.pageX;
     const wid = window.innerWidth;
     const percent = parseInt((curPos/wid) *200);
     console.log(percent);
-    
+
     //parseInt(숫자) : 실수에서 소수점 아랫값을 버려서 정수 반환
     //parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
 })
+
+//인수로 갯수를 받아 동적으로 Img 생성해 주는 함수
+function createImgs(target, num){
+    for(let i=0; i < num; i++){
+        const img = document.createElement('img');
+        const src = document.createAttribute('src');
+        src.value = `img/pic${i}.jpg`;
+        img.setAttributeNode(src);
+        target.append(img);
+    }
+    return target.querySelectorAll('img');  
+}
