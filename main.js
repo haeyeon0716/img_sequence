5b9ddf4dce3a23eae9dc1b0cff233eff0f23481a
yhy7805 <yhy7805@gmail.com>
2023-09-21 16:32:37 +0900

    이미지 없을시에도 대체 이미지 처리

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const section = document.querySelector('section');
 const imgs = createImgs(section, num);
 const loadingNum = document.querySelector('aside p span');
 const aside = document.querySelector('aside');
+const delay = convertSpeed(aside);
 
 
 
@@ -28,25 +29,35 @@ function createImgs(target, num){
     const imgs = target.querySelectorAll('img');
     let count =0;
     imgs.forEach((img)=>{
-        //해당 도메 수반되는 소스 이미지가 로딩완료시 실행되는 이벤트
+        //만약 이미지요소의 소스 이미지에 문제 발생시 대체 이미지 처리
+        img.onerror =() =>{
+            img.setAttribute('src', 'img/thumb1.jpg');
+        };
+
         img.onload =()=>{
             count++;
             const percent = parseInt((count / num) * 100);
             loadingNum.innerText = percent;
-            console.log('현재 로딩된 소스 이미지', count);
             if(count === num){
-                //동적으로 만들어진 img 요소의 소스 이미지가 렌더링 완료된 시점
                 console.log('모든 소스이미지 로딩 완료');
-                aside.remove;
+                aside.classList.add('off');
+                setTimeout(()=>{
+                    aside.remove();
+                }, delay)
             }
         };
     });
     return imgs;  
 }
 
+//인수로 transition-Duration값을 구해야 하는 DOM요소를 전달받음
+function convertSpeed(el){
+    //해당요소의 transition-Duration가뵤을 쟁연산해서 가져온 다음 숫자로 바꾸고 *1000을 해서 밀리세컨드 형태로 반환
+    const result = getComputedStyle(el).transitionDuration;
+    return parseFloat(result) * 1000;
+}
+
 function activation(arr , index){
     arr.forEach((el)=>(el.style.display = 'none'));
     arr[index].style.display = 'block';
 }
-
-
