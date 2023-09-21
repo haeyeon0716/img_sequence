12241cfc33c13eaa672cfc8f5804a0e3a76dfcd6
yhy7805 <yhy7805@gmail.com>
2023-09-21 14:34:55 +0900

    이미지 순번에 따른 동작  활성화

--- main.js
+++ main.js
@@ -15,20 +15,24 @@
 //백분율 구하는 공식
 //현재 수치값 / 전체수치값 * 100 (백분율)
 //현재 수치값 / 전체수치값 * 200 (이백분율)
-
+const num =200;
 const section = document.querySelector('section');
-const imgs = createImgs(section, 200);
-console.log(imgs);
+const imgs = createImgs(section, num);
+
+
 
 window.addEventListener('mousemove' , (e)=>{
+    const percent = getPercent(e, 200)
+    activation(imgs, percent);
+})
+
+//이벤트정보 객체와 전체갯수를 받아서 
+//해당 숫자에 대한 백분율 반환함수
+function getPercent(e, num){
     const curPos = e.pageX;
     const wid = window.innerWidth;
-    const percent = parseInt((curPos/wid) *200);
-    console.log(percent);
-
-    //parseInt(숫자) : 실수에서 소수점 아랫값을 버려서 정수 반환
-    //parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
-})
+    return parseInt((curPos/wid) * num);
+}
 
 //인수로 갯수를 받아 동적으로 Img 생성해 주는 함수
 function createImgs(target, num){
@@ -40,4 +44,12 @@ function createImgs(target, num){
         target.append(img);
     }
     return target.querySelectorAll('img');  
+}
+
+//인수로 그룹 유사배열, 활성화 순번을 받아서 
+//해당 순번의 요소만 활성화 처리
+function activation(arr , index){
+    arr.forEach((el)=>(el.style.display = 'none'));
+    arr[index].style.display = 'block';
+
 }
